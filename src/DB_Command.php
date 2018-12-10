62886b43a21642f912af3c9865dfb8cd6f62be6f
Patrick Garman <patrick@garman.io>
2018-12-10 11:10:34 -0600

    —human-readable should not allow plain numeric repsonses

--- src/DB_Command.php
+++ src/DB_Command.php
@@ -885,7 +885,7 @@ class DB_Command extends WP_CLI_Command {
 			}
 		}
 
-		if ( ! empty( $size_format) && ! $tables && ! $format ) {
+		if ( ! empty( $size_format) && ! $tables && ! $format && ! $human_readable ) {
 			WP_CLI::Line( filter_var( $rows[0]['Size'], FILTER_SANITIZE_NUMBER_INT ) );
 		} else {
 			// Display the rows.
